fb2be264f6e8993ec1ab623c36b4a4cc9f3f1237
maretzky85 <maretzky85@gmail.com>
2019-07-06 11:58:17 +0200

    added test profile activation in tests

--- src/test/java/com/sikoramarek/fiszki/AbstractTest.java
+++ src/test/java/com/sikoramarek/fiszki/AbstractTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -22,6 +23,7 @@ import java.io.IOException;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = FiszkiApplication.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public abstract class AbstractTest {
 
 	protected MockMvc mvc;
