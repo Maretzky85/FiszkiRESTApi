48d357060dc91d3a9d0e27035c085e77e351d7bb
Grzegorz Banaszek <grzegorz.banaszek@gmail.com>
2019-07-12 09:04:53 +0200

    refactored answer tests, Added checking content during edition. Tests still use data from sql file

--- src/test/java/com/sikoramarek/fiszki/controller/AnswerControllerTest.java
+++ src/test/java/com/sikoramarek/fiszki/controller/AnswerControllerTest.java
@@ -4,7 +4,6 @@ import com.sikoramarek.fiszki.AbstractTest;
 import com.sikoramarek.fiszki.UserType;
 import com.sikoramarek.fiszki.model.Answer;
 import com.sikoramarek.fiszki.repository.AnswerRepository;
-import org.junit.After;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
@@ -13,153 +12,105 @@ import static org.junit.Assert.assertEquals;
 
 public class AnswerControllerTest extends AbstractTest {
 
-    private Long answerId;
-    private Answer answer;
     private String uri = "/answers/";
-    private int status;
-    private MvcResult mvcResult;
-    private String requestJson;
 
     @Autowired
     private AnswerRepository answerRepository;
 
-    @After
-    public void setDown() {
-        answer = answerRepository.findAnswerById(answerId);
-        System.out.println("Answer after update: " + answer);
-    }
-
     @Test
-    public void editAnswerByIdByLoggedUserExpects200() throws Exception {
-        answerId = 2L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = mapToJson(answer);
-        mvcResult = performPut(uri + answerId, requestJson, UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
+    public void editAnswerByLoggedUserExpects200() throws Exception {
+        assertEquals(200, editAnswerStatus(2L, UserType.USER, ""));
     }
 
     @Test
     public void editAnswerByIdByAdminExpects200() throws Exception {
-        answerId = 1L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = mapToJson(answer);
-        mvcResult = performPut(uri + answerId, requestJson, UserType.ADMIN);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
+        assertEquals(200, editAnswerStatus(1L, UserType.ADMIN, ""));
     }
-
 
     @Test
     public void editAnswerByIdByUnloggedUserExpects401() throws Exception {
-        answerId = 1L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = super.mapToJson(answer);
-        mvcResult = performPut(uri + answerId, requestJson, UserType.UNLOGGED);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(401, status);
+        assertEquals(401, editAnswerStatus(1L, UserType.UNLOGGED, ""));
     }
 
     @Test
     public void editAnswerByIdByLoggedUserExpects404() throws Exception {
-        answerId = 2L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = super.mapToJson(answer);
-        mvcResult = performPut(uri + "1444", requestJson, UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(404, status);
+        assertEquals(404, editAnswerStatus(2L, UserType.USER, "14444"));
     }
 
     @Test
     public void editAnswerByIdByAdminExpects404() throws Exception {
-        answerId = 1L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = super.mapToJson(answer);
-        mvcResult = performPut(uri + answerId + "1444", requestJson, UserType.ADMIN);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(404, status);
+        assertEquals(404, editAnswerStatus(1L, UserType.ADMIN, "14444"));
     }
 
     @Test
     public void editAnswerByIdByLoggedUserExpects400() throws Exception {
-        answerId = 2L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = super.mapToJson(answer);
-        mvcResult = performPut(uri + answerId + ":user", requestJson, UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(400, status);
+        assertEquals(400, editAnswerStatus(2L, UserType.USER, ":user"));
     }
 
     @Test
     public void editAnswerByIdByAdminExpects400() throws Exception {
-        answerId = 1L;
-        answer = answerRepository.findAnswerById(answerId);
-        answer.setAnswer("Nowa odpowiedz");
-        requestJson = super.mapToJson(answer);
-        mvcResult = performPut(uri + answerId + ":user", requestJson, UserType.ADMIN);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(400, status);
+        assertEquals(400, editAnswerStatus(1L, UserType.ADMIN, ":user"));
     }
 
     @Test
     public void deleteAnswerByIdByLoggedUserExpects200() throws Exception {
-        answerId = 74L;
-        mvcResult = performDelete(uri + answerId, UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
+        assertEquals(200, deleteAnswerStatus(74L, UserType.USER, ""));
     }
 
     @Test
     public void deleteAnswerByIdByAdminExpects200() throws Exception {
-        answerId = 76L;
-        mvcResult = performDelete(uri + answerId, UserType.ADMIN);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(200, status);
+        assertEquals(200, deleteAnswerStatus(76L, UserType.ADMIN, ""));
     }
 
     @Test
     public void deleteAnswerByIdByUnloggedUserExpects401() throws Exception {
-        answerId = 77L;
-        mvcResult = performDelete(uri + answerId, UserType.UNLOGGED);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(401, status);
+        assertEquals(401, deleteAnswerStatus(77L, UserType.UNLOGGED, ""));
     }
 
     @Test
     public void deleteAnswerByIdByLoggedUserAndNoOwnerExpects403() throws Exception {
-        answerId = 87L;
-        mvcResult = performDelete(uri + answerId, UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(403, status);
+        assertEquals(403, deleteAnswerStatus(87L, UserType.USER, ""));
     }
 
     @Test
     public void deleteAnswerByIdByLoggedUserAndNoAdminExpects403() throws Exception {
-        answerId = 88L;
-        mvcResult = performDelete(uri + answerId, UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(403, status);
+        assertEquals(403, deleteAnswerStatus(88L, UserType.USER, ""));
     }
 
     @Test
     public void deleteAnswerByIdByLoggedUserAndAnswerNotExistsExpects404() throws Exception {
-        answerId = 88L;
-        mvcResult = performDelete(uri + answerId + "222222", UserType.USER);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(404, status);
+        assertEquals(404, deleteAnswerStatus(88L, UserType.USER, "2222222"));
     }
 
     @Test
     public void deleteAnswerByIdByAdminAndAnswerNotExistsExpects404() throws Exception {
-        answerId = 88L;
-        mvcResult = performDelete(uri + answerId + "222222", UserType.ADMIN);
-        status = mvcResult.getResponse().getStatus();
-        assertEquals(404, status);
+        assertEquals(404, deleteAnswerStatus(88L, UserType.ADMIN, "2222222"));
+    }
+
+    private int editAnswerStatus(Long answerId, UserType userType, String postfix) throws Exception {
+        Answer answer = answerRepository.findAnswerById(answerId);
+
+        String beforeAnswerContents = answer.getAnswer();
+        String newAnswerContents = "New answer";
+        answer.setAnswer(newAnswerContents);
+        String requestJson = mapToJson(answer);
+        MvcResult mvcResult = performPut(uri + answerId + postfix, requestJson, userType);
+        int status = mvcResult.getResponse().getStatus();
+
+        answer = answerRepository.findAnswerById(answerId);
+        if (status != 200) {
+            System.out.println("Status different than 200 " + answer.getAnswer());
+            assertEquals(beforeAnswerContents, answer.getAnswer());
+        } else {
+            System.out.println("Status equals 200 " + answer.getAnswer());
+            assertEquals(newAnswerContents, answer.getAnswer());
+        }
+
+        return status;
+    }
+
+    private int deleteAnswerStatus(Long answerId, UserType userType, String postfix) throws Exception {
+        MvcResult mvcResult = performDelete(uri + answerId + postfix, userType);
+        return mvcResult.getResponse().getStatus();
     }
 }
