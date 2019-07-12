3af0f47cbd96cf9de267615d83f8cc8db64ef1c8
Grzegorz Banaszek <grzegorz.banaszek@gmail.com>
2019-07-12 22:03:32 +0200

    Test/answer (#3)
    * Tests for answers

--- src/test/java/com/sikoramarek/fiszki/AbstractTest.java
+++ src/test/java/com/sikoramarek/fiszki/AbstractTest.java
@@ -6,9 +6,8 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sikoramarek.fiszki.model.Role;
 import com.sikoramarek.fiszki.model.UserModel;
-import com.sikoramarek.fiszki.repository.UserRepository;
+import com.sikoramarek.fiszki.repository.*;
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,8 +24,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.Filter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -39,176 +36,182 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 public abstract class AbstractTest {
 
-	protected MockMvc mvc;
+    protected MockMvc mvc;
 
-	@Autowired
-	WebApplicationContext webApplicationContext;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
-	@Autowired
-	private Filter springSecurityFilterChain;
-	@Autowired
-	BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private Filter springSecurityFilterChain;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	private String USER_TOKEN;
-	private String loginUri = "/login";
-	private String userName = "user";
-	private String adminName = "admin";
-	private String password = "123456";
-	private String ADMIN_TOKEN;
+    private String USER_TOKEN;
+    private String loginUri = "/login";
+    private String userName = "user";
+    private String adminName = "admin";
+    private String password = "123456";
+    private String ADMIN_TOKEN;
 
-	private static boolean initialized = false;
+    private static boolean initialized = false;
 
+    @Autowired
+    protected UserRepository userRepository;
 
-	@Autowired
-	UserRepository userRepository;
+    @Autowired
+    protected QuestionRepository questionRepository;
 
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-				.addFilters(springSecurityFilterChain).build();
-		if (!initialized){
-			initUsers();
+    @Autowired
+    protected AnswerRepository answerRepository;
 
-		}
+    @Autowired
+    protected TagRepository tagRepository;
 
-		USER_TOKEN = getToken(userName);
-		ADMIN_TOKEN = getToken(adminName);
-	}
+    @Autowired
+    protected RoleRepository roleRepository;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilters(springSecurityFilterChain).build();
 
-	protected String mapToJson(Object obj) throws JsonProcessingException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		return objectMapper.writeValueAsString(obj);
-	}
+        if (!initialized) {
+            initUsers();
+        }
+        USER_TOKEN = getToken(userName);
+        ADMIN_TOKEN = getToken(adminName);
+    }
 
-	protected <T> T mapFromJson(String json, Class<T> clazz)
-			throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		return objectMapper.readValue(json, clazz);
-	}
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(obj);
+    }
 
-	private String getToken(String userName) throws Exception {
-		String jsonLogin = "{\"username\":\"" + userName + "\", \"password\":\"" + password + "\"}";
-		MvcResult mvcResultLogin = mvc.perform(MockMvcRequestBuilders.post(loginUri)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(jsonLogin))
-				.andExpect(status().isOk()).andReturn();
-		String fullToken = mvcResultLogin.getResponse().getHeader(HEADER_STRING);
-		return fullToken.replace(HEADER_STRING, "");
-	}
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
+    }
 
-	public MvcResult performGet(String uri, UserType userType) throws Exception {
-		if (userType == UserType.USER) {
-			return mvc.perform(MockMvcRequestBuilders.get(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.header(HEADER_STRING, USER_TOKEN))
-					.andReturn();
-		}
-		if (userType == UserType.ADMIN) {
-			return mvc.perform(MockMvcRequestBuilders.get(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.header(HEADER_STRING, ADMIN_TOKEN))
-					.andReturn();
-		}
-		if (userType == UserType.UNLOGGED) {
-			return mvc.perform(MockMvcRequestBuilders.get(uri)
-					.contentType(MediaType.APPLICATION_JSON))
-					.andReturn();
-		}
-		throw new RuntimeException("Unknown user type");
+    private String getToken(String userName) throws Exception {
+        String jsonLogin = "{\"username\":\"" + userName + "\", \"password\":\"" + password + "\"}";
+        MvcResult mvcResultLogin = mvc.perform(MockMvcRequestBuilders.post(loginUri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonLogin))
+                .andExpect(status().isOk()).andReturn();
+        String fullToken = mvcResultLogin.getResponse().getHeader(HEADER_STRING);
+        return fullToken.replace(HEADER_STRING, "");
+    }
 
-	}
+    public MvcResult performGet(String uri, UserType userType) throws Exception {
+        switch (userType) {
+            case USER:
+                return mvc.perform(MockMvcRequestBuilders.get(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HEADER_STRING, USER_TOKEN))
+                        .andReturn();
+            case ADMIN:
+                return mvc.perform(MockMvcRequestBuilders.get(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HEADER_STRING, ADMIN_TOKEN))
+                        .andReturn();
+            case UNLOGGED:
+                return mvc.perform(MockMvcRequestBuilders.get(uri)
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andReturn();
+            default:
+                throw new RuntimeException("Unknown user type");
+        }
+    }
 
-	public MvcResult performPost(String uri, String jsonContent, UserType userType) throws Exception {
-		if (userType == UserType.USER) {
-			return mvc.perform(MockMvcRequestBuilders.post(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.header(HEADER_STRING, USER_TOKEN)
-					.content(jsonContent))
-					.andReturn();
-		}
-		if (userType == UserType.ADMIN) {
-			return mvc.perform(MockMvcRequestBuilders.post(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.header(HEADER_STRING, ADMIN_TOKEN)
-					.content(jsonContent))
-					.andReturn();
-		}
-		if (userType == UserType.UNLOGGED) {
-			return mvc.perform(MockMvcRequestBuilders.post(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(jsonContent))
-					.andReturn();
-		}
-		throw new RuntimeException("Unknown user type");
-	}
+    public MvcResult performPost(String uri, String jsonContent, UserType userType) throws Exception {
+        switch (userType) {
+            case USER:
+                return mvc.perform(MockMvcRequestBuilders.post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HEADER_STRING, USER_TOKEN)
+                        .content(jsonContent))
+                        .andReturn();
+            case ADMIN:
+                return mvc.perform(MockMvcRequestBuilders.post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HEADER_STRING, ADMIN_TOKEN)
+                        .content(jsonContent))
+                        .andReturn();
+            case UNLOGGED:
+                return mvc.perform(MockMvcRequestBuilders.post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent))
+                        .andReturn();
+            default:
+                throw new RuntimeException("Unknown user type");
+        }
+    }
 
-	public MvcResult performPut(String uri, String jsonContent, UserType userType) throws Exception {
-		if (userType == UserType.USER) {
-			return mvc.perform(MockMvcRequestBuilders.put(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.header(HEADER_STRING, USER_TOKEN)
-					.content(jsonContent)).andReturn();
-		}
-		if (userType == UserType.ADMIN) {
-			return mvc.perform(MockMvcRequestBuilders.put(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.header(HEADER_STRING, ADMIN_TOKEN)
-					.content(jsonContent)).andReturn();
-		}
-		if (userType == UserType.UNLOGGED) {
-			return mvc.perform(MockMvcRequestBuilders.put(uri)
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(jsonContent)).andReturn();
-		}
-		throw new RuntimeException("Unknown user type");
-	}
+    public MvcResult performPut(String uri, String jsonContent, UserType userType) throws Exception {
+        switch (userType) {
 
-	public MvcResult performDelete(String uri, UserType userType) throws Exception {
-		if (userType == UserType.USER) {
-			return mvc.perform(MockMvcRequestBuilders.delete(uri)
-					.header(HEADER_STRING, USER_TOKEN)
-					.contentType(MediaType.APPLICATION_JSON)).andReturn();
-		}
-		if (userType == UserType.ADMIN) {
-			return mvc.perform(MockMvcRequestBuilders.delete(uri)
-					.header(HEADER_STRING, ADMIN_TOKEN)
-					.contentType(MediaType.APPLICATION_JSON)).andReturn();
-		}
-		if (userType == UserType.UNLOGGED) {
-			return mvc.perform(MockMvcRequestBuilders.delete(uri)
-					.contentType(MediaType.APPLICATION_JSON)).andReturn();
-		}
-		throw new RuntimeException("Unknown user type");
-	}
+            case USER:
+                return mvc.perform(MockMvcRequestBuilders.put(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HEADER_STRING, USER_TOKEN)
+                        .content(jsonContent)).andReturn();
+            case ADMIN:
+                return mvc.perform(MockMvcRequestBuilders.put(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HEADER_STRING, ADMIN_TOKEN)
+                        .content(jsonContent)).andReturn();
+            case UNLOGGED:
+                return mvc.perform(MockMvcRequestBuilders.put(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonContent)).andReturn();
+            default:
+                throw new RuntimeException("Unknown user type");
+        }
+    }
 
-	private void initUsers(){
-		Role userRole = new Role();
-		userRole.setId(2L);
-		userRole.setRole("USER");
-		Role adminRole = new Role();
-		adminRole.setId(1L);
-		adminRole.setRole("ADMIN");
-		Set<Role> roles = new HashSet<>();
-		roles.add(userRole);
-		UserModel userModel = new UserModel();
-		userModel.setUsername(userName);
-		userModel.setPassword(bCryptPasswordEncoder.encode(password));
-		userModel.setRoles(roles);
-		userModel.setEmail("blabla@blabla.com");
-		userRepository.save(userModel);
+    public MvcResult performDelete(String uri, UserType userType) throws Exception {
+        switch (userType) {
+            case USER:
+                return mvc.perform(MockMvcRequestBuilders.delete(uri)
+                        .header(HEADER_STRING, USER_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)).andReturn();
+            case ADMIN:
+                return mvc.perform(MockMvcRequestBuilders.delete(uri)
+                        .header(HEADER_STRING, ADMIN_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)).andReturn();
+            case UNLOGGED:
+                return mvc.perform(MockMvcRequestBuilders.delete(uri)
+                        .contentType(MediaType.APPLICATION_JSON)).andReturn();
+            default:
+                throw new RuntimeException("Unknown user type");
+        }
+    }
 
-		roles.add(adminRole);
-		UserModel userModel2 = new UserModel();
-		userModel2.setUsername(adminName);
-		userModel2.setPassword(bCryptPasswordEncoder.encode(password));
-		userModel2.setEmail("blabla2@blabla.com");
-		userModel2.setRoles(roles);
-		userRepository.save(userModel2);
-		initialized = true;
-	}
+    private void initUsers() {
+        Role userRole = new Role();
+        userRole.setId(2L);
+        userRole.setRole("USER");
+        Role adminRole = new Role();
+        adminRole.setId(1L);
+        adminRole.setRole("ADMIN");
+        Set<Role> roles = new HashSet<>();
+        roles.add(userRole);
+        UserModel userModel = new UserModel();
+        userModel.setUsername(userName);
+        userModel.setPassword(bCryptPasswordEncoder.encode(password));
+        userModel.setRoles(roles);
+        userModel.setEmail("blabla@blabla.com");
+        userRepository.save(userModel);
+
+        roles.add(adminRole);
+        UserModel userModel2 = new UserModel();
+        userModel2.setUsername(adminName);
+        userModel2.setPassword(bCryptPasswordEncoder.encode(password));
+        userModel2.setEmail("blabla2@blabla.com");
+        userModel2.setRoles(roles);
+        userRepository.save(userModel2);
+        initialized = true;
+    }
 }
 
---
+++ src/test/java/com/sikoramarek/fiszki/controller/AnswerControllerTest.java
@@ -0,0 +1,126 @@
+package com.sikoramarek.fiszki.controller;
+
+import com.sikoramarek.fiszki.AbstractTest;
+import com.sikoramarek.fiszki.UserType;
+import com.sikoramarek.fiszki.model.Answer;
+import com.sikoramarek.fiszki.model.Question;
+import com.sikoramarek.fiszki.model.Tag;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.sikoramarek.fiszki.UserType.USER;
+import static org.junit.Assert.assertEquals;
+
+public class AnswerControllerTest extends AbstractTest {
+
+    private String uri = "/answers/";
+    private Long rowCount = 5L;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        prepareData();
+    }
+
+    private void prepareData() throws Exception {
+
+        for (Long i = 1L; i < rowCount; i++) {
+            Tag tag = new Tag();
+            tag.setId(i);
+            tag.setTagName("test" + i);
+            tagRepository.save(tag);
+
+            Set<Tag> tags = new HashSet<>();
+            tags.add(tag);
+
+            Question question = new Question();
+            question.setAccepted(true);
+            question.setQuestion("Question" + i);
+            question.setTitle("Question title" + i);
+            question.setTags(tags);
+            String jsonPost = mapToJson(question);
+            performPost("/questions", jsonPost, USER);
+
+            Answer answer = new Answer();
+            answer.setAnswer("Answer " + i);
+            answer.setQuestion(question);
+            jsonPost = mapToJson(answer);
+
+            performPost("/questions/"+ i + "/answers", jsonPost, USER);
+        }
+    }
+
+    @Test
+    public void editAnswerExpects200() throws Exception {
+        assertEquals(200, editAnswerStatus(1L, UserType.USER, ""));
+        assertEquals(200, editAnswerStatus(2L, UserType.ADMIN, ""));
+    }
+
+    @Test
+    public void editAnswerExpects401() throws Exception {
+        assertEquals(401, editAnswerStatus(3L, UserType.UNLOGGED, ""));
+    }
+
+    @Test
+    public void editAnswerExpects404() throws Exception {
+        assertEquals(404, editAnswerStatus(4L, UserType.USER, "14444"));
+        assertEquals(404, editAnswerStatus(5L, UserType.ADMIN, "14444"));
+    }
+
+    @Test
+    public void editAnswerExpects400() throws Exception {
+        assertEquals(400, editAnswerStatus(6L, UserType.USER, ":user"));
+        assertEquals(400, editAnswerStatus(7L, UserType.ADMIN, ":user"));
+    }
+
+    @Test
+    public void deleteAnswerExpects200() throws Exception {
+        assertEquals(200, deleteAnswerStatus(8L, UserType.USER, ""));
+        assertEquals(200, deleteAnswerStatus(9L, UserType.ADMIN, ""));
+    }
+
+    @Test
+    public void deleteAnswerExpects401() throws Exception {
+        assertEquals(401, deleteAnswerStatus(10L, UserType.UNLOGGED, ""));
+    }
+
+    @Test
+    public void deleteAnswerExpects403() throws Exception {
+        assertEquals(403, deleteAnswerStatus(11L, UserType.UNLOGGED, ""));
+    }
+
+    @Test
+    public void deleteAnswerExpects404() throws Exception {
+        assertEquals(404, deleteAnswerStatus(13L, UserType.USER, "2222222"));
+        assertEquals(404, deleteAnswerStatus(14L, UserType.ADMIN, "2222222"));
+    }
+
+    private int editAnswerStatus(Long answerId, UserType userType, String postfix) throws Exception {
+        Answer answer = answerRepository.findAnswerById(answerId);
+
+        String beforeAnswerContents = answer.getAnswer();
+        String newAnswerContents = "New answer";
+        answer.setAnswer(newAnswerContents);
+        String requestJson = mapToJson(answer);
+        MvcResult mvcResult = performPut(uri + answerId + postfix, requestJson, userType);
+        int status = mvcResult.getResponse().getStatus();
+
+        answer = answerRepository.findAnswerById(answerId);
+        if (status != 200) {
+            assertEquals(beforeAnswerContents, answer.getAnswer());
+        } else {
+            assertEquals(newAnswerContents, answer.getAnswer());
+        }
+
+        return status;
+    }
+
+    private int deleteAnswerStatus(Long answerId, UserType userType, String postfix) throws Exception {
+        MvcResult mvcResult = performDelete(uri + answerId + postfix, userType);
+        return mvcResult.getResponse().getStatus();
+    }
+}
